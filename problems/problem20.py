3fbbce646fc5b056df8fe803bada4142878533be
phanghos <tatasciorecont@gmail.com>
2014-06-30 00:08:20 -0430

    Solution to problem 20

---
+++ problems/problem20.py
@@ -0,0 +1,17 @@
+def factorial(n):
+	f = 1
+	for i in range(n, 1, -1):
+		f *= i
+		
+	return f
+	
+def sum_digits(n):
+	s = 0
+	while n > 0:
+		div = n // 10
+		s += n - div * 10
+		n = div
+		
+	return s
+	
+print(sum_digits(factorial(100)))
